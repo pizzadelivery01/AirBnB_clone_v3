fe1bd1a104529e3716c2bc0a59b22f1b9eb23953
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-29 18:32:26 -0400

    [update] added Flaske to import

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response
+from flask import Flask, jsonify, abort, request, make_response
 from models import storage
 from models.state import State
 
