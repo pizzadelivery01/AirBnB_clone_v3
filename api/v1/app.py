faa54ba892b8fb6b2372b659a6f073b29b26f8b3
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-29 15:16:28 -0400

    [update] updated teh app.run settings

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,6 +24,6 @@ def page_not_found(e):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
-    host = os.environ.get('HBNB_API_HOST') or '0.0.0.0'
-    port = os.environ.get('HBNB_API_PORT') or '5000'
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = os.environ.get('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
