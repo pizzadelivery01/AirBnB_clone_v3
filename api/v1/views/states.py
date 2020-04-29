99bf6597a426262d9873312627f2906ae2bd840f
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-29 17:45:39 -0400

    [update] changed returns to aborts and reworked a few options

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from models import storage
 from models.state import State
 
@@ -8,26 +8,31 @@ from models.state import State
 @app_views.route('/states', methods=['GET'])
 def statesGet():
     """ states """
-    allList = [x.to_dict() for x in storage.all(State).items()]
+    allList = []
+    objs = storage.all(State)
+    for x,y in objs.items():
+        allList.append(y.to_dict())
     return jsonify(allList)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'])
 def statesGetId(state_id):
     """ states """
-    theState = storage.get(State, state_id)
-    if theState:
-        return jsonify(theState.to_dict())
+    target = "State." + str(state_id)
+    allList = []
+    if target in storage.all(State).keys():
+        return jsonify(storage.all(State)[target].to_dict())
     else:
         abort(404)
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
-def statesDeleteId(state_id):
+def statesDeleteId(state_id=None):
     """ states """
     theState = storage.get(State, state_id)
     if theState:
-        storage.delete(theState)
+        theState.delete()
+        storage.save()
         return jsonify({}), 200
     else:
         abort(404)
@@ -36,21 +41,21 @@ def statesDeleteId(state_id):
 @app_views.route('/states', methods=['POST'])
 def statesPost():
     """ states """
-    print('test')
     requested = request.get_json()
     if requested:
         if 'name' in requested:
             newState = State(**requested)
-            storage.new(newState)
+            newState.save()
         else:
-            return 'Missing name', 400
+            abort,(400, "Missing name")
     else:
-        return 'Not a JSON', 400
+        abort(400, "Not a JSON")
     return jsonify(newState.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
-def statesPutId(state_id):
+def statesPutId(state_id=None):
+    """ updates """
     ignore = ['id', 'created_at', 'updated_at']
     theState = storage.get(State, state_id)
     if theState:
@@ -61,7 +66,7 @@ def statesPutId(state_id):
                     setattr(theState, x, y)
             theState.save()
         else:
-            return 'Not a JSON', 400
+            abort(400, "Not a JSON")
         return jsonify(theState.to_dict()), 200
     else:
         abort(404)
