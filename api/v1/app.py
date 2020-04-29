dab03cc46721f0b5acb0b6fdd45c74c76e4acae8
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-29 15:22:29 -0400

    [update] changed to getenv

--- api/v1/app.py
+++ api/v1/app.py
@@ -24,6 +24,5 @@ def page_not_found(e):
     return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
-    host = os.environ('HBNB_API_HOST', '0.0.0.0')
-    port = os.environ('HBNB_API_PORT', '5000')
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=os.getenv('HBNB_API_PORT', '5000'), threaded=True)
