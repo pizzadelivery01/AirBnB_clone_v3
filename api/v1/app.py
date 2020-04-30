f2a22057f2383738beadb4232fb65628411a6df1
Jeremy Bedolla <jjbedolla@gmail.com>
2020-04-30 15:38:16 -0400

    [update] added cors funtionality

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,14 +4,13 @@ from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
-
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app, origins='0.0.0.0')
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
 
 @app.teardown_appcontext
 def dbColose(error):
